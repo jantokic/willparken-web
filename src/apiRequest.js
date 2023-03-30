7457ca5e63a5f1530d930c667aa5efbe1a2f7101
Jan Tokic <jan.tokic@htl-wels.at>
2023-03-30 14:56:46 +0200

    Jan - reverted back to localhost

--- src/apiRequest.js
+++ src/apiRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const api = async (url, method = 'get', data = null, headers = {}) => {
   // change url from localhost:3000 to https://seal-app-jk939.ondigitalocean.app/
-  url = url.replace('http://localhost:3000', 'https://seal-app-jk939.ondigitalocean.app');
+  // url = url.replace('http://localhost:3000', 'https://seal-app-jk939.ondigitalocean.app');
   
   const config = {
     method: method,
